851508a86ef45155563d5293a31f349686af6e36
Leonel <leonel_lima19@hotmail.com>
2021-09-18 17:57:56 -0300

    Header for Sidebar

--- src/components/Aside/Head/index.js
+++ src/components/Aside/Head/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import {
     Container,
+    GroupTitle,
+    Button,
 } from './styles';
 
 const Head = () => {
     return (
         <Container>
-            
+            <GroupTitle>Slack Clon</GroupTitle>
+            <Button></Button>
         </Container>
     )
 }
--- src/components/Aside/Head/styles.js
+++ src/components/Aside/Head/styles.js
@@ -3,5 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.div`
     height: 50px;
     min-height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 16px;
     border-bottom: 1px solid ${p => p.theme.colors['violet']['border']};
+`;
+
+export const GroupTitle = styled.h1`
+    color: white;
+    font-size: 20px;
+`;
+
+export const Button = styled.button`
+    width: 34px;
+    height: 34px;
+    border: none;
+    border-radius: 50%;
+    background-color: white;
+    &:focus {
+        outline: none;
+    }
 `;
