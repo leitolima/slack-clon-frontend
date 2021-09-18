b29b026b08b1d47ade0fc5b05cd867f0b9277467
Leonel <leonel_lima19@hotmail.com>
2021-09-18 16:57:02 -0300

    Theme with styled components

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        dark: {
+            'sidebar-text': '#ADABAF',
+            'message-text': '#D1D2D3',
+            'section-background': '#1a1d21',
+        },
+        violet: {
+            'border': '#2c2831',
+            'sidebar-background': '#14101A',
+            'selected': '#6715eb',
+            'input-background': 'rgba(255, 255, 255, 0.2)',
+        }
+    }
+}
