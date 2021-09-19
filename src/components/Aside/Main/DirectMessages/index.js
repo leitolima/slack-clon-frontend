c936500ce62a48ef3270a293c287a09e65e085d8
Leonel <leonel_lima19@hotmail.com>
2021-09-19 13:00:19 -0300

    Same styles for Direct Messages button

--- src/components/Aside/Main/DirectMessages/index.js
+++ src/components/Aside/Main/DirectMessages/index.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight, faEllipsisV, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import {
     Container,
     TitleContainer,
+    Flex,
     Title,
+    Button,
 } from './styles';
 
 const DirectMessages = () => {
 
     const [open, setOpen] = useState(false);
+    const [hover, setHover] = useState(false);
 
     return (
         <Container>
-            <TitleContainer>
-                <Title>Mensajes directos</Title>
+            <TitleContainer
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                onClick={() => setOpen(!open)}
+            >
+                <Flex>
+                    <Button show open={open}>
+                        <FontAwesomeIcon className="caret" icon={faCaretRight}/>
+                    </Button>
+                    <Title>Mensajes directos</Title>
+                </Flex>
+                <Flex>
+                    <Button hover={hover}>
+                        <FontAwesomeIcon icon={faEllipsisV}/>
+                    </Button>
+                    <Button hover={hover}>
+                        <FontAwesomeIcon icon={faPlus}/>
+                    </Button>
+                </Flex>
             </TitleContainer>
         </Container>
     )
